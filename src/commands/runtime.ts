ca4e60886f85371750df162cc14fb54a6d8ea648
SpikeHD <spikegdofficial@gmail.com>
2022-11-10 20:37:51 -0800

    runtime selection commands

--- src/commands/runtime.ts
+++ src/commands/runtime.ts
@@ -1,4 +1,4 @@
-import { commands, window, workspace } from "vscode";
+import { commands, ConfigurationTarget, window, workspace } from "vscode";
 import * as fs from 'fs';
 
 export function runtimeSelect() {
@@ -6,12 +6,12 @@ export function runtimeSelect() {
     const path = workspace.getConfiguration('gmbuilder').get('runtimePath') as fs.PathLike;
     
     if (!fs.existsSync(path)) {
-      window.showErrorMessage('GMBuilder runtime path is not set, or is set incorrectly!');
+      window.showErrorMessage('GMBuilder runtime path is not set, or is set incorrectly! Set this option in the extensions settings.');
       commands.executeCommand('workbench.action.openSettings');
       return;
     }
 
-    // const runtimes = fs.readdirSync();
+    await runtimeQuickPick(false);
   });
 }
 
@@ -20,9 +20,34 @@ export function betaRuntimeSelect() {
     const path = workspace.getConfiguration('gmbuilder').get('betaRuntimePath') as fs.PathLike;
     
     if (!fs.existsSync(path)) {
-      window.showErrorMessage('GMBuilder runtime path is not set, or is set incorrectly!');
+      window.showErrorMessage('GMBuilder beta runtime path is not set, or is set incorrectly! Set this option in the extensions settings.');
       commands.executeCommand('workbench.action.openSettings');
       return;
     }
+
+    await runtimeQuickPick(true);
   });
+}
+
+/**
+ * Create a QuickPick that allows selecting a runtime dynamically
+ * 
+ * @param beta Whether this should set the beta or regular runtime
+ * @returns 
+ */
+async function runtimeQuickPick(beta: boolean) {
+  const gmBuilder = workspace.getConfiguration('gmbuilder');
+  const folder = beta ? gmBuilder.get('betaRuntimePath') : gmBuilder.get('runtimePath');
+  const runtimes = fs.readdirSync(folder as fs.PathLike) || ['none'];
+
+  const result = await window.showQuickPick(runtimes, {
+    placeHolder: 'Select a runtime'
+  });
+
+  // No runtimes were available
+  if (result === 'none') {
+    return;
+  }
+
+  gmBuilder.update(beta ? 'selectedBetaRuntime' : 'selectedRuntime', result, ConfigurationTarget.Global);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -2,18 +2,13 @@ import * as vscode from 'vscode';
 import { runtimeSelect, betaRuntimeSelect } from './commands/runtime';
 import { firstTimeSetup, registerRuntimeChangeEventHandler } from './common/setup';
 
-export function activate(context: vscode.ExtensionContext) {
-	// Settings are by-workspace
-	if (!vscode.workspace.workspaceFolders) {
-		return;
-	}
+// This sets the runtime when a runtime path is first added
+registerRuntimeChangeEventHandler();
 	
-	// This will run some stuff if none of the config options are set
-	firstTimeSetup();
+// This will run some stuff if none of the config options are set
+firstTimeSetup();
 
-	// This sets the runtime when a runtime path is first added
-	registerRuntimeChangeEventHandler();
-
+export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(runtimeSelect());
 	context.subscriptions.push(betaRuntimeSelect());
 }
