98913aa458e5ce8942e7c5ae546be9f66e4c0939
SpikeHD <spikegdofficial@gmail.com>
2022-11-10 19:05:44 -0800

    cleanup

---
+++ src/commands/runtime.ts
@@ -0,0 +1,26 @@
+import { commands, window, workspace } from "vscode";
+import * as fs from 'fs';
+
+export function runtimeSelect() {
+  return commands.registerCommand('gmbuilder.selectRuntime', async () => {
+    const path = workspace.getConfiguration('gmbuilder').get('runtimePath') as fs.PathLike;
+    
+    if (!fs.existsSync(path)) {
+      window.showErrorMessage('GMBuilder runtime path is not set, or is set incorrectly!');
+      commands.executeCommand('workbench.actions.openSettings', 'gmbuilder');
+      return;
+    }
+  });
+}
+
+export function betaRuntimeSelect() {
+  return commands.registerCommand('gmbuilder.selectBetaRuntime', async () => {
+    const path = workspace.getConfiguration('gmbuilder').get('betaRuntimePath') as fs.PathLike;
+    
+    if (!fs.existsSync(path)) {
+      window.showErrorMessage('GMBuilder runtime path is not set, or is set incorrectly!');
+      commands.executeCommand('workbench.actions.openSettings', 'gmbuilder');
+      return;
+    }
+  });
+}
